e63810b78bdca0aef8c8b124bf537d7f9c1bc48f
logixSystems <logixsystems6@gmail.com>
2025-08-04 19:08:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
+import {locales} from './navigation';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'pt'],
+  locales,
  
   // Used when no locale matches
   defaultLocale: 'en',
