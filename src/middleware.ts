90de9c23487471252481fa2108a4dfce19bb272e
logixSystems <logixsystems6@gmail.com>
2025-08-05 20:39:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
-import i18nConfig from '../i18n';
-
+import { locales, defaultLocale } from '../i18n';
+ 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: i18nConfig.locales,
+  locales,
  
   // Used when no locale matches
-  defaultLocale: i18nConfig.defaultLocale,
+  defaultLocale,
 
   localePrefix: 'as-needed'
 });
