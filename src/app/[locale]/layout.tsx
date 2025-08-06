fdab684e85ac835652e3cc02a3d5b6166d4419ed
logixSystems <logixsystems6@gmail.com>
2025-08-06 20:04:23 +0000

    account:30 [Intervention] Slow network is detected. See https://www.chro

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,17 +3,18 @@ import {getMessages} from 'next-intl/server';
  
 export default async function LocaleLayout({
   children,
-  params: {locale}
+  params
 }: {
   children: React.ReactNode;
   params: {locale: string};
 }) {
+  const {locale} = params;
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
  
   return (
-    <NextIntlClientProvider messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       {children}
     </NextIntlClientProvider>
   );
