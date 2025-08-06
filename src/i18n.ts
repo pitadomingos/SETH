db06a4103e96d491ba3441850b3276117b247bc5
logixSystems <logixsystems6@gmail.com>
2025-08-06 12:03:29 +0000

    I think this issues lies with the firebase itself, not functional. Can y

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
  
 export const locales = ['en', 'pt'];
 export const defaultLocale = 'en';
-
+ 
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) {
-      // Handle invalid locale, maybe redirect or use a default
-  }
-
+  if (!locales.includes(locale as any)) notFound();
+ 
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
