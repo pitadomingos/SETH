f101688c2d1f14937585e33f961b4a73af3ae9e3
logixSystems <logixsystems6@gmail.com>
2025-08-06 19:44:08 +0000

    ✅ Correct Usage for next-intl App Router To fix it, ensure the following

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,17 +1,19 @@
-import { ReactNode } from "react";
-import { NextIntlClientProvider, useMessages } from "next-intl";
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
  
-export default function LocaleLayout({
+export default async function LocaleLayout({
   children,
-  params,
+  params: {locale}
 }: {
-  children: ReactNode;
-  params: { locale: string };
+  children: React.ReactNode;
+  params: {locale: string};
 }) {
-  const messages = useMessages();
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
  
   return (
-    <NextIntlClientProvider locale={params.locale} messages={messages}>
+    <NextIntlClientProvider messages={messages}>
       {children}
     </NextIntlClientProvider>
   );
