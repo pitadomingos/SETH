02f66a2dc4038f83bbd55080aeb86c5829d78b03
logixSystems <logixsystems6@gmail.com>
2025-08-10 08:11:50 +0000

    still 404 page with url /en. I thought I mentioned the default MUST be /

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,12 @@ import createMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale } from './i18n';
  
 export default createMiddleware({
-  // A list of all locales that are supported
   locales,
- 
-  // Used when no locale matches
-  defaultLocale
+  defaultLocale,
+  localePrefix: 'always'
 });
  
 export const config = {
   // Match only internationalized pathnames
-  // The negative lookahead is to exclude API routes and static files
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import {redirect} from 'next/navigation';
 import {defaultLocale} from '../../i18n';
  
-// This page only renders for the root `/` URL
-// and redirects to the default locale.
 export default function RootPage() {
   redirect(`/${defaultLocale}`);
 }
