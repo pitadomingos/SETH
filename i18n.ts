4a6e2fe35e2606ddce73fca2ece716a7323b7c08
logixSystems <logixsystems6@gmail.com>
2025-08-11 09:52:15 +0000

    Ok, Are you sure that we will get out alive out of this problem. Take yo

--- i18n.ts
+++ i18n.ts
@@ -1,9 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
  
-export const locales = ['pt', 'en'];
+export const locales = ['en', 'pt'];
 export const defaultLocale = 'pt';
  
 export default getRequestConfig(async ({locale}) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
+ 
   return {
     messages: (await import(`./src/messages/${locale}.json`)).default
   };
