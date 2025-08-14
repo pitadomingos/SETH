9aaa274d7b8ba03f5976bd78e72d592b0b8e5020
logixSystems <logixsystems6@gmail.com>
2025-08-14 07:13:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- i18n.ts
+++ i18n.ts
@@ -1,12 +1,14 @@
-import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
+import {notFound} from 'next/navigation';
  
 export const locales = ['en', 'pt'];
 export const defaultLocale = 'pt';
  
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locales.includes(locale as any)) {
+    notFound();
+  }
  
   return {
     messages: (await import(`./src/messages/${locale}.json`)).default
