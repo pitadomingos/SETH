b9f75ed220893ef9199b91c67eb3b8f5851f4604
logixSystems <logixsystems6@gmail.com>
2025-08-15 13:16:18 +0000

    Console added fro  middleware for cloud run

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale } from './i18n';
  
 export default createMiddleware({
+  console.log('Locales in middleware:', locales);
   locales: [...locales],
   defaultLocale,
   localePrefix: 'always'
