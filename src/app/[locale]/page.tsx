c1a10295c477cb29a450cd2707a4414191bfeffc
logixSystems <logixsystems6@gmail.com>
2025-08-19 13:47:12 +0000

    2025-08-19T13:44:29Z [web]  ✓ Starting... 2025-08-19T13:44:29Z [web] <Fi

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import { LoginForm } from '@/components/auth/login-form';
-import { ThemeProvider } from '@/components/layout/theme-provider';
 
 export default function LoginPage() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-            <LoginForm />
-        </main>
-    </ThemeProvider>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
+        <LoginForm />
+    </main>
   );
 }
