6be69dca7c336178839b47133a817aa121c42458
logixSystems <logixsystems6@gmail.com>
2025-08-19 13:15:31 +0000

    Why is it not running in this environment. I am getting 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,6 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'seth.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  output: 'standalone',
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
