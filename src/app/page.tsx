83ef8082d58bed5927e743f904b32b3debb30c56
logixSystems <logixsystems6@gmail.com>
2025-08-19 13:17:25 +0000

    My question is why the prewview still shwining 404

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import {redirect} from 'next/navigation';
-import {defaultLocale} from '../i18n';
-
-// This page only renders for the root `/` URL
-export default function RootPage() {
-  redirect(defaultLocale);
-}
